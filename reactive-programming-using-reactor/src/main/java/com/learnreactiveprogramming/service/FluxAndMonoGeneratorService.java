bd01ef6508cb959b971f3c3d309e3540089c41d8
Joliver Estampador <joliverestampador@gmail.com>
2023-04-13 07:59:44 +0800

    First Mono

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,6 +1,7 @@
 package com.learnreactiveprogramming.service;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -9,13 +10,23 @@ public class FluxAndMonoGeneratorService {
         // creating a Flux
         return Flux.fromIterable(List.of("Alex", "Joli", "Popoy")); // imagine we retrieved this in a db
     }
+
+    public Mono<String> nameMono() {
+        // creating a Mono
+        return Mono.just("Alex");
+    }
     public static void main(String[] args) {
         FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
         // Only way to access the value, "subscribe" to the Flux
         // If you dont subscribe, there will be no data to access
         fluxAndMonoGeneratorService.namesFlux()
                 .subscribe(name -> {
-                    System.out.println("Name is: " + name);
+//                    System.out.println("Name is: " + name);
                 }); // will be sent in a string one by one
+
+        fluxAndMonoGeneratorService.nameMono()
+                .subscribe(name -> {
+                    System.out.println("Name is: " + name);
+                });
     }
 }
