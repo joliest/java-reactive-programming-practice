06f379fea76b003f6730bcf106acddd47d545afa
Joliver Estampador <joliverestampador@gmail.com>
2023-04-13 07:43:37 +0800

    Basic way in accessing the Flux values

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,21 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
 public class FluxAndMonoGeneratorService {
+    public Flux<String> namesFlux() {
+        // creating a Flux
+        return Flux.fromIterable(List.of("Alex", "Joli", "Popoy")); // imagine we retrieved this in a db
+    }
+    public static void main(String[] args) {
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+        // Only way to access the value, "subscribe" to the Flux
+        // If you dont subscribe, there will be no data to access
+        fluxAndMonoGeneratorService.namesFlux()
+                .subscribe(name -> {
+                    System.out.println("Name is: " + name);
+                }); // will be sent in a string one by one
+    }
 }
