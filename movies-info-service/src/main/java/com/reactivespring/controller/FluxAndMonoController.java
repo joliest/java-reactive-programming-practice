8cb6871799c224bba03bd0e2c575a520aaa1491a
Joliver Estampador <joliverestampador@gmail.com>
2023-05-04 08:55:41 +0800

    My first stream

--- movies-info-service/src/main/java/com/reactivespring/controller/FluxAndMonoController.java
+++ movies-info-service/src/main/java/com/reactivespring/controller/FluxAndMonoController.java
@@ -1,9 +1,12 @@
 package com.reactivespring.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @RestController
 public class FluxAndMonoController {
@@ -14,5 +17,12 @@ public class FluxAndMonoController {
     @GetMapping("/mono")
     public Mono<String> mono() {
         return Mono.just("Hello").log();
+    }
+    // produces = MediaType.TEXT_EVENT_STREAM_VALUE instruct endpoint to produce a stream of data to the client
+    @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Long> stream() {
+        // continously send some events with an interval of x
+        // for every second, it will send update to the client
+        return Flux.interval(Duration.ofSeconds(1)).log();
     }
 }
