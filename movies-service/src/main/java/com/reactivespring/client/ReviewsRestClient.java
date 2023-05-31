62fe5e66b911bf5c3af2cff47fd40f311258f87a
Joliver Estampador <joliverestampador@gmail.com>
2023-05-31 08:01:59 +0800

    Creating WebClient for Reviews

---
+++ movies-service/src/main/java/com/reactivespring/client/ReviewsRestClient.java
@@ -0,0 +1,34 @@
+package com.reactivespring.client;
+
+import com.reactivespring.domain.Review;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
+
+@Component
+public class ReviewsRestClient {
+    private WebClient webClient;
+    public ReviewsRestClient(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+
+    @Value("${restClient.reviewsUrl}")
+    private String reviewsUrl;
+
+    public Flux<Review> retrieveReviews(String movieId) {
+        var uri = UriComponentsBuilder.fromHttpUrl(reviewsUrl)
+                .queryParam("movieInfoId", movieId)
+                .buildAndExpand()
+                .toString();
+
+        return webClient
+                .get()
+                .uri(uri)
+                .retrieve()
+                .bodyToFlux(Review.class);
+
+    }
+}
