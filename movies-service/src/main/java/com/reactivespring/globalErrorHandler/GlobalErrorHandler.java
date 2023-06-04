76cb99ece22336fcbcefc6dac5310c52cf00fa07
Joliver Estampador <joliverestampador@gmail.com>
2023-06-04 08:43:28 +0800

    Creating Global Error Handler

---
+++ movies-service/src/main/java/com/reactivespring/globalErrorHandler/GlobalErrorHandler.java
@@ -0,0 +1,23 @@
+package com.reactivespring.globalErrorHandler;
+
+
+import com.reactivespring.exception.MoviesInfoClientException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalErrorHandler {
+
+    /**
+     * Corrects the error 500 Internal Server Error we get by default.
+     * See com/reactivespring/client/MoviesInfoRestClient.java:30 (onStatus) for implementation sample
+     */
+    @ExceptionHandler(MoviesInfoClientException.class)
+    public ResponseEntity<String> handleClientException(MoviesInfoClientException exception) {
+        log.error("Exception Caught in handleClientException : {}", exception.getMessage());
+        return ResponseEntity.status(exception.getStatusCode()).body(exception.getMessage());
+    }
+}
