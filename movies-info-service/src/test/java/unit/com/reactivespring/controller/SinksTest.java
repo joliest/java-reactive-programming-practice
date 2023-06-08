ad526b70c54795e811c0f3f33394108a62c94f61
Joliver Estampador <joliverestampador@gmail.com>
2023-06-08 08:37:01 +0800

    multicast()
    https://docs.google.com/document/d/1SpIS9hyMTOC6xuT2LLurWiCVbOroLb-VhLY-sek8hco/edit#heading=h.o8oo1mubwgd6

--- movies-info-service/src/test/java/unit/com/reactivespring/controller/SinksTest.java
+++ movies-info-service/src/test/java/unit/com/reactivespring/controller/SinksTest.java
@@ -6,7 +6,7 @@ import reactor.core.publisher.Sinks;
 
 public class SinksTest {
     @Test
-    void sink() {
+    void sinks_replay() {
         // Creating a Sink
         // this sink will emit or publish multiple events
         // anytime a subscriber is connected into it will go to replay all the events
@@ -38,5 +38,29 @@ public class SinksTest {
         integerFlux3.subscribe((i) -> {
             System.out.println("Subscriber 3 : " + i);
         });
+    }
+
+    @Test
+    // transmits only the newly published data
+    void sinks_multicast() {
+        // you need to add onBackpressureBuffer - newly pushed as in "after the subscriber's subscription"
+        Sinks.Many<Integer> multicast = Sinks.many().multicast().onBackpressureBuffer();
+
+        // subscriber 1
+        Flux<Integer> integerFlux = multicast.asFlux();
+        integerFlux.subscribe((i) -> {
+            System.out.println("Subscriber 1 : " + i);
+        });
+
+        // emitting events
+        multicast.emitNext(1, Sinks.EmitFailureHandler.FAIL_FAST);
+        multicast.emitNext(2, Sinks.EmitFailureHandler.FAIL_FAST);
+
+        // another subscriber
+        Flux<Integer> integerFlux2 = multicast.asFlux();
+            integerFlux2.subscribe((i) -> {
+            System.out.println("Subscriber 2 : " + i);
+        });
+        multicast.emitNext(3, Sinks.EmitFailureHandler.FAIL_FAST);
     }
 }
