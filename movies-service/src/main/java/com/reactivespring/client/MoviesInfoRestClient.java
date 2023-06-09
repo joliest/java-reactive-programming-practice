ac1e8105c8d9f451c16ab6227fd45ef4deca3513
Joliver Estampador <joliverestampador@gmail.com>
2023-06-09 08:05:44 +0800

    Implement stream endpoint in the Movie Service client

--- movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
+++ movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
@@ -1,5 +1,6 @@
 package com.reactivespring.client;
 
+import com.reactivespring.domain.Movie;
 import com.reactivespring.domain.MovieInfo;
 import com.reactivespring.exception.MoviesInfoClientException;
 import com.reactivespring.exception.MoviesInfoServerException;
@@ -10,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.Exceptions;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.util.retry.Retry;
 
@@ -60,6 +62,36 @@ public class MoviesInfoRestClient {
                 // retry the call for 3x if the call fails
 //                .retry(3)
                 .retryWhen(RetryUtil.retrySpec())
+                .log();
+    }
+
+    public Flux<MovieInfo> retrieveMovieInfoStream() {
+        var url = movieInfoUrl.concat("/stream");
+        return webClient
+                .get()
+                .uri(url)
+                .retrieve()
+                .onStatus(HttpStatus::is4xxClientError,  clientResponse -> {
+                    log.info("Status code is: {}", clientResponse.statusCode().value());
+                    if (clientResponse.statusCode().equals(HttpStatus.NOT_FOUND)) {
+                        return Mono.error(new MoviesInfoClientException(
+                                "There is no MovieInfo available",
+                                clientResponse.statusCode().value()
+                        ));
+                    }
+                    return clientResponse.bodyToMono(String.class)
+                            .flatMap(responseMessage -> Mono.error(new MoviesInfoClientException(
+                                    responseMessage, clientResponse.statusCode().value()
+                            )));
+                })
+                .onStatus(HttpStatus::is5xxServerError,  clientResponse -> {
+                    log.info("Status code is: {}", clientResponse.statusCode().value());
+                    return clientResponse.bodyToMono(String.class)
+                            .flatMap(responseMessage -> Mono.error(new MoviesInfoServerException(
+                                    "Server error in MovieInfoService " + responseMessage
+                            )));
+                })
+                .bodyToFlux(MovieInfo.class)
                 .log();
     }
 }
